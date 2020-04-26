33551b0878f6294b53a4464474868ec91ebe1616
Alexander <e-urezu@mail.ru>
2020-04-26 14:22:22 +0300

    Add Test 2.5.1 Strings

--- Tutorials/Test_2_5_1.go
+++ Tutorials/Test_2_5_1.go
@@ -1,10 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
 
 func main() {
-	var s string = "Это строка"
-	for _, b := range s {
-		fmt.Print(string(b))
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	rn := []rune(text)
+	var count int
+	if unicode.IsUpper(rn[0]) {
+		count++
 	}
+	if string(rn[len(rn)-1]) == "." {
+		count++
+	}
+	if count == 2 {
+		fmt.Println("Right")
+	} else {
+		fmt.Println("Wrong")
+	}
+	fmt.Println("Счётчик: ", count)
+	fmt.Print("Длинна: ", len(text))
 }
